562d5456c690ef9248d687ec406b219c431b2e86
lacey <hiu@example.com>
2018-11-27 15:30:54 +0000

    corrections to test 1 part B

--- bus.rb
+++ bus.rb
@@ -1,14 +1,30 @@
 class Bus
 
-attr_reader :route_number, :destination
+attr_reader :route_number, :destination, :passengers
 
-  def initialize(route_number, destination)
+  def initialize(route_number, destination, passengers)
     @route_number = route_number
     @destination = destination
+    @passengers = []
   end
 
   def bus_can_drive()
     return "Brum brum"
   end
+
+  def number_of_passengers
+    @passengers = []
+    return @passengers.count
+  end
+
+  # def add_passenger_to_bus(person, age)
+  #   passengers << person_1
+  # end
+
+
+
+
+
+
 
 end
--- specs/bus_spec.rb
+++ specs/bus_spec.rb
@@ -2,11 +2,13 @@ require("minitest/autorun")
 require("minitest/rg")
 
 require_relative("../bus")
+require_relative("../person")
 
 class BusTest < MiniTest::Test
 
   def setup
-    @bus_1 = Bus.new("22", "Ocean Terminal")
+    # person_1 = Person.new("Doris", 101)
+    @bus_1 = Bus.new("22", "Ocean Terminal", [])
   end
 
   def test_bus_has_route
@@ -21,6 +23,14 @@ class BusTest < MiniTest::Test
     assert_equal("Brum brum", @bus_1.bus_can_drive)
   end
 
+  def test_number_of_passengers_on_bus
+    assert_equal(0, @bus_1.number_of_passengers)
+  end
 
+  # def test_add_passenger_to_bus
+  #   # @bus_1.add_passenger_to_bus("Doris", 101)
+  #   assert_equal(@passengers, @bus_1.add_passenger_to_bus("Doris", 101))
+  # end
+  #
 
 end
